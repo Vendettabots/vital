e63c0c65c1b66d345e3ccd23c76930c4515166a6
Vendettabots <vendettabots@proton.me>
2025-06-11 12:55:41 -0500

    Create card.tsx

---
+++ components/ui/card.tsx
@@ -0,0 +1,9 @@
+export function Card({ children }) {
+  return (
+    <div className="rounded-xl shadow-md bg-white">{children}</div>
+  );
+}
+
+export function CardContent({ children, className = "" }) {
+  return <div className={`p-4 ${className}`}>{children}</div>;
+}
